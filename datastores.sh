5c31694c922d78e34dc27189639c96458b4dde4c
Miles Monteleone <scratchyseal@gmail.com>
2019-10-03 16:26:02 -0700

    Update datastores.sh
    Look at me @abianco3 committing directly to master

--- datastores.sh
+++ datastores.sh
@@ -27,7 +27,7 @@ brew install mysql@5.7
 
 # Install mysql workbench
 # Install Cask
-brew install caskroom/cask/brew-cask
+#brew install caskroom/cask/brew-cask
 brew cask install --appdir="/Applications" mysqlworkbench
 
 # Remove outdated versions from the cellar.
